4c57ab7a22d42d82475f3c29532226818b073530
Zenith Anthony <dabarerawza.19@uom.lk>
2022-04-14 19:54:16 +0530

    typograph added

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -1,15 +1,23 @@
 import Grid from '@material-ui/core/Grid';
  import Paper from '@material-ui/core/Paper';
+ import Typography from '@material-ui/core/Typography';
+
+
 
 
 
  const TourCard = () => {
    return ( <Grid item xs={3}>
-    <Paper elevation={3}>1</Paper>
-   
+    <Paper elevation={3}><img className='img'src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e" alt="" />
+    <Typography variant='h6' component="h1">
+    ji
+    </Typography>
+    </Paper>  
+    
+
    </Grid>)
 
-
+ 
 
  }
  
