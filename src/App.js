961eb739f876bc960b711b55dd6ebf916b9a5d8a
Zenith Anthony <dabarerawza.19@uom.lk>
2022-04-14 18:11:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import TourCard from "./components/TourCard";
+import Container from '@material-ui/core/Container';
+ import Grid from '@material-ui/core/Grid';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Container>
+
+    <Grid container spacing ={10}>
+    <TourCard/>
+    <TourCard/> 
+     <TourCard/> 
+     <TourCard/>
+     <TourCard/>
+     <TourCard/>
+     <TourCard/>
+      
+    
+
+    </Grid>
+    </Container>
     </div>
   );
 }
---
+++ src/components/TourCard.js
@@ -0,0 +1,17 @@
+import Grid from '@material-ui/core/Grid';
+ import Paper from '@material-ui/core/Paper';
+
+
+
+ const TourCard = () => {
+   return ( <Grid item xs={3}>
+    <Paper elevation={3}>1</Paper>
+   
+   </Grid>)
+
+
+
+ }
+ 
+ export default TourCard;
+ 
