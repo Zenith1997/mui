48c39498771f8f0db78ddfb8c2907476b2874724
Zenith Anthony <dabarerawza.19@uom.lk>
2022-04-15 01:04:33 +0530

    icon added

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -1,25 +1,41 @@
-import Grid from '@material-ui/core/Grid';
- import Paper from '@material-ui/core/Paper';
- import Typography from '@material-ui/core/Typography';
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Box from '@mui/material/Box';
+import { AccessTime } from "@mui/icons-material";
+import { margin, width } from "@mui/system";
 
+const TourCard = () => {
+  return (
+    <Grid item xs={4}>
+      <Paper variant="outlined" elevation={3}>
+        <img
+          className="img"
+          src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e"
+          alt=""
+        />
+        <Box  paddingX={2}>
+        <Typography variant="subtitle2" component="h2">
+        Immerse into the falls
+      </Typography>
+        </Box>
+       <Box
+       sx={{
+           display:"flex",
+           alignItems:"center",
+           marginLeft:2
+        
+       }}
+       >
+       <AccessTime  sx={{width:15}}/>
+       <Typography marginLeft={1} variant="subtitle2" component="h2">
+       5 hours
+     </Typography>
+   
+       </Box>
+      </Paper>
+    </Grid>
+  );
+};
 
-
-
-
- const TourCard = () => {
-   return ( <Grid item xs={3}>
-    <Paper elevation={3}><img className='img'src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e" alt="" />
-    <Typography variant='h6' component="h1">
-    ji
-    </Typography>
-    </Paper>  
-    
-
-   </Grid>)
-
- 
-
- }
- 
- export default TourCard;
- 
+export default TourCard;
